4450d344119edede3d6538778a8bdcd06562c52e
UKHYUN <UKHYUN@192.168.10.102>
2015-02-23 01:54:23 +0900

    OnDestroy 에다가 port닫는 부분 추가.

--- ESC_Project/src/com/esc_project/productManager/ProductManagerActivity.java
+++ ESC_Project/src/com/esc_project/productManager/ProductManagerActivity.java
@@ -1,21 +1,12 @@
 package com.esc_project.productManager;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Random;
 
-import android.content.Context;
-import android.hardware.usb.UsbManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
+import android.widget.ListView;
 
 import com.esc_project.Constants;
 import com.esc_project.R;
@@ -26,37 +17,34 @@ import com.esc_project.Connection.SocketHelper;
 public class ProductManagerActivity extends StartActivity {
 
 	// Git Hub TEST!!
-	ProductListAdaptor mTestListAdapter;
+	ProductListAdaptor productListAdapter;
 	
 	SocketHelper mSocketHelper;
 	subAsyncTask mSubAsyncTask;
-	
-	
 	
 	ProductManager productManager;
 	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_test);
 	    
 	    productManager = new ProductManager( this );
+	    ArrayList<String> test = new ArrayList ( ); 
+	    ListView listview = new ListView(this);
+	    productListAdapter = new ProductListAdaptor(this);
+	    listview.setAdapter(productListAdapter);
 	    
 	    productManager.OpenSerialPort();
-	   
-	    ArrayList<String> test = new ArrayList ( ); 
-//	    ListView listview = new ListView(this);
-//	    mTestListAdapter = new TestListAdapter(this);
-//	    listview.setAdapter(mTestListAdapter);
 	    
-	   mSocketHelper = mSocketHelper.getInstance(getApplicationContext());
+	    mSocketHelper = mSocketHelper.getInstance(getApplicationContext());
 	   
-	   mSubAsyncTask = new subAsyncTask();
-	   mSubAsyncTask.execute();
+	    mSubAsyncTask = new subAsyncTask();
+	    mSubAsyncTask.execute();
 	    
-	   actList.add(this);
-	
+	    actList.add(this);
 	}
 	
 	private class subAsyncTask extends AsyncTask<Void, Void, Void> {
@@ -65,9 +53,7 @@ public class ProductManagerActivity extends StartActivity {
 		protected Void doInBackground(Void... message) {
 			// TODO Auto-generated method stub
 			
-//			TEST_Method();
 
-			// String형 List = RFID UID
 			ArrayList<String> taggedUIDs = new ArrayList<String>();
 			taggedUIDs = productManager.GetTaggedUIDs();
 
@@ -77,11 +63,15 @@ public class ProductManagerActivity extends StartActivity {
 			mSocketHelper.sendMessage(mHandler, str_json);
 			/**처리부분 **/
 
-//			TEST_Method_CLEAN();
-
 			return null;
 		}
 		
+	}
+	
+	@Override
+	protected void onDestroy(){
+		this.productManager.CloseSerialPort();
+		super.onDestroy();
 	}
 	
 	Handler mHandler = new Handler() {
